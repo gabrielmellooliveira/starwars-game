bad43dfa7f50a5cb7ef339848d2d934071e96a31
Gabriel Mello de Oliveira <gabrielmello3011@gmail.com>
2019-12-08 15:11:41 -0300

    Update credentials.js

--- src/services/custom-search/credentials.js
+++ src/services/custom-search/credentials.js
@@ -1,4 +1,4 @@
 export const credentials = {
-    apiKey: "AIzaSyCK-by97nhVIOMdrOY7mW1T2sigCQqmbqI",
-    searchEngineId: "005660643243188123298:1mfkbzow6oj"
+    apiKey: "your-apikey",
+    searchEngineId: "your-searchengineid"
 }
